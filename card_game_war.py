eb1b7e08b25c9e60618eb8bd268f2f292b9aad45
domantasdi <72785432+domantasdi@users.noreply.github.com>
2024-03-02 19:56:08 +0200

    black passthrough

--- card_game_war.py
+++ card_game_war.py
@@ -21,50 +21,56 @@ Description:
         '''
 """
 
+
 class Card:
     """
     A blueprint for a card object.
     :param rank: the rank (value) of a card.
     """
-    rank = [2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9,
-            10,
-            11,
-            12,
-            13,
-            14,
+
+    rank = [
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
     ]
 
     # Do I really need this?
     def __init__(self, rank):
         self.rank = rank
+
     pass
 
 
 class Deck:
     """Placeholder docstring 1"""
+
     deck = []
-    #def __init__(self):
+    # def __init__(self):
     pass
 
 
 class Player:
     """Placeholder docstring 1"""
-    #def __init__(self):
-    #def get_name()
+
+    # def __init__(self):
+    # def get_name()
     pass
 
 
 def main():
     """Placeholder docstring 1"""
-    #player1 = Player._
-    #player2 = Player.__
+    # player1 = Player._
+    # player2 = Player.__
     pass
 
 
