91e6e6465abeee12d7bfb8bee325eb38a2674219
domantasdi <72785432+domantasdi@users.noreply.github.com>
2024-03-02 22:35:35 +0200

    Removed unnecessary pass statements, added the .gitignore file

--- card_game_war.py
+++ card_game_war.py
@@ -25,30 +25,15 @@ Description:
 class Card:
     """
     A blueprint for a card object.
+    
     :param rank: the rank (value) of a card.
     """
 
-    rank = [
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        12,
-        13,
-        14,
-    ]
+    rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
     # Do I really need this?
     def __init__(self, rank):
         self.rank = rank
-
-    pass
 
 
 class Deck:
@@ -56,7 +41,6 @@ class Deck:
 
     deck = []
     # def __init__(self):
-    pass
 
 
 class Player:
@@ -64,14 +48,12 @@ class Player:
 
     # def __init__(self):
     # def get_name()
-    pass
 
 
 def main():
     """Placeholder docstring 1"""
     # player1 = Player._
     # player2 = Player.__
-    pass
 
 
 if __name__ == "__main__":
