03cc3a5a616107180663a48b79065c82e094158f
domantasdi <72785432+domantasdi@users.noreply.github.com>
2024-03-02 17:50:06 +0200

    Added some initial classes
    Placeholders for the following classes: Card, Deck, Player. Brain doesn't work well today.

--- card-game-war.py
+++ card-game-war.py
@@ -1,16 +1,39 @@
 class Card:
+    value = [2,
+             3,
+             4,
+             5,
+             6,
+             7,
+             8,
+             9,
+             10,
+             11,
+             12,
+             13,
+             14,        
+    ]
+    def __init__(self, value):
+        self.value = value
+    
     pass
 
 
 class Deck:
+    deck = []
+    #def __init__(self):
     pass
 
 
 class Player:
+    #def __init__(self):  
+    #def get_name()
     pass
 
 
 def main():
+    #player1 = Player._
+    #player2 = Player.__
     pass
 
 
