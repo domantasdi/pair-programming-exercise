59d0b02b5c14fd4c4df5aa4ec7b63323cec403a4
domantasdi <72785432+domantasdi@users.noreply.github.com>
2024-03-02 19:43:13 +0200

    Conforming to snake_case convention, added placeholder docstrings

--- card_game_war.py
+++ card_game_war.py
@@ -1,8 +1,9 @@
+"""Placeholder docstring"""
+
 class Card:
     """
     A blueprint for a card object.
-    
-    param: rank – the rank (value) of a card.
+    :param rank: the rank (value) of a card.
     """
     rank = [2,
              3,
@@ -16,29 +17,31 @@ class Card:
              11,
              12,
              13,
-             14,        
+             14,
     ]
 
     # Do I really need this?
     def __init__(self, rank):
         self.rank = rank
-    
     pass
 
 
 class Deck:
+    """Placeholder docstring 1"""
     deck = []
     #def __init__(self):
     pass
 
 
 class Player:
-    #def __init__(self):  
+    """Placeholder docstring 1"""
+    #def __init__(self):
     #def get_name()
     pass
 
 
 def main():
+    """Placeholder docstring 1"""
     #player1 = Player._
     #player2 = Player.__
     pass
